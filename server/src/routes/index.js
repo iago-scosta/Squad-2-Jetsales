e267d5983be66cee90d0df89feca3af8a6338ebe
EricVMoura <ericmoura709@gmail.com>
2026-05-17 13:26:04 -0300

    1. ajeitei o docker compose e removi a especificidade da versao 2. consegui criar as entidades conversation e messages respectivamente   3.atualizei a versao do node para a gente usar no package 4.criei o arquivo migrations(agora é possivel fazer migraçoes usando dps de ter dado o build no docker (npm run migrate:latest)

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const chatbotRoutes = require('../modules/chatbot/chatbot.routes');
 const messageRoutes = require('../modules/conversation/message.routes');
+const conversationRoutes = require('../modules/conversation/conversation.routes');
 
 router.use('/chatbots', chatbotRoutes);
 router.use('/messages', messageRoutes);
+router.use('/conversations', conversationRoutes);
 
 module.exports = router;
